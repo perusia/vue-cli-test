79ea59337cf728251af53559885192af9f9e7a83
SuzukiTakumi <suzuki@nakaoakirayuunoMacBook-puro.local>
2019-10-11 17:03:53 +0900

    イベントで処理するまで

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <p v-if="msg.length > 0">
+      {{msg}}
+    </p>
+    <p v-else>
+      no text
+    </p>
+    <input type="text" v-model="msg">
+    <button @click="clear()">clear</button>
+  </div>
+</template>
+
+<script>
+export default{
+  data(){
+    return{
+      msg:'Hello World!'
+    }
+  },
+  methods:{
+    clear(){
+      this.msg=' '
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
